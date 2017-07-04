2bbcb2d7cfc8c89f75fafdb79e8211b9374643e5
Dave Kuhn <david.s.kuhn@gmail.com>
2017-07-04 11:14:00 +1000

    Load polyfills on demand

--- generators/app/templates/src/main/static/javascript/index.jsx
+++ generators/app/templates/src/main/static/javascript/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
-import '../less/styles/main.less';
+import loadPolyfills from './polyfills';
 import App from './components/App';
+import '../less/styles/main.less';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -20,10 +20,12 @@ const render = (Component) => {
   );
 };
 
-// Perform initial render
-render(App);
+loadPolyfills(() => {
+  // Perform initial render
+  render(App);
 
-// Wire in Hot Module Replacement
-if (module.hot) {
-  module.hot.accept('./components/App', () => render(App));
-}
+  // Wire in Hot Module Replacement
+  if (module.hot) {
+    module.hot.accept('./components/App', () => render(App));
+  }
+});
