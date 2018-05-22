b37b2839b7de59b66f302c525c9b4732923aabcd
Matt Byrne <matt@3wks.com.au>
2018-05-22 12:47:24 +1000

    Update npm dependency versions where it's easy.

--- generators/app/templates/src/main/client/javascript/components/common/ReactSelect.jsx
+++ generators/app/templates/src/main/client/javascript/components/common/ReactSelect.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import * as PropTypes from 'prop-types';
 import { Chip, MenuItem, Typography } from 'material-ui';
@@ -10,45 +10,41 @@ import ArrowDropUpIcon from 'material-ui-icons/ArrowDropUp';
 import ReactDOM from 'react-dom';
 
 class Option extends React.Component {
-    propTypes = {
-      children: PropTypes.array.isRequired,
-      isFocused: PropTypes.array.isRequired,
-      isSelected: PropTypes.array.isRequired,
-      onFocus: PropTypes.array.isRequired,
-      onSelect: PropTypes.func.isRequired,
-      option: PropTypes.object.isRequired,
-    };
+  static propTypes = {
+    children: PropTypes.array.isRequired,
+    isFocused: PropTypes.array.isRequired,
+    isSelected: PropTypes.array.isRequired,
+    onFocus: PropTypes.array.isRequired,
+    onSelect: PropTypes.func.isRequired,
+    option: PropTypes.object.isRequired,
+  };
 
-    defaultValues = {
+  handleClick = (event) => {
+    this.props.onSelect(this.props.option, event);
+  };
 
-    }
+  render() {
+    const {
+      children,
+      isFocused,
+      isSelected,
+      onFocus,
+    } = this.props;
 
-    handleClick = (event) => {
-      this.props.onSelect(this.props.option, event);
-    };
-
-    render() {
-      const {
-        children,
-        isFocused,
-        isSelected,
-        onFocus,
-      } = this.props;
-
-      return (
-        <MenuItem
-          onFocus={onFocus}
-          selected={isFocused}
-          onClick={this.handleClick}
-          component="div"
-          style={{
-            fontWeight: isSelected ? 500 : 400,
-          }}
-        >
-          {children}
-        </MenuItem>
-      );
-    }
+    return (
+      <MenuItem
+        onFocus={onFocus}
+        selected={isFocused}
+        onClick={this.handleClick}
+        component="div"
+        style={{
+          fontWeight: isSelected ? 500 : 400,
+        }}
+      >
+        {children}
+      </MenuItem>
+    );
+  }
 }
 
 // https://github.com/JedWatson/react-select/issues/810
@@ -69,15 +65,15 @@ class SelectWithPortal extends Select {
         className="Select-menu-outer"
         onClick={(e) => { e.stopPropagation(); }}
         style={{
-            ...this.props.menuContainerStyle,
-            zIndex: 9999,
-            position: 'absolute',
-            width: dimensions.width,
-            top: dimensions.top + dimensions.height,
-            left: dimensions.left,
-            maxHeight: Math.min(maxHeight, 200),
-            overflow: 'hidden',
-          }}
+          ...this.props.menuContainerStyle,
+          zIndex: 9999,
+          position: 'absolute',
+          width: dimensions.width,
+          top: dimensions.top + dimensions.height,
+          left: dimensions.left,
+          maxHeight: Math.min(maxHeight, 200),
+          overflow: 'hidden',
+        }}
       >
         <div
           ref={(ref) => { this.menu = ref; }}
@@ -86,9 +82,9 @@ class SelectWithPortal extends Select {
           className="Select-menu"
           id={`${instancePrefix}-list`}
           style={{
-              ...this.props.menuStyle,
-              maxHeight: Math.min(maxHeight, 200),
-            }}
+            ...this.props.menuStyle,
+            maxHeight: Math.min(maxHeight, 200),
+          }}
           onScroll={this.handleMenuScroll}
           onMouseDown={this.handleMouseDownOnMenu}
         >
@@ -118,28 +114,28 @@ const SelectWrapped = (props) => {
       arrowRenderer={arrowRenderer}
       clearRenderer={() => <ClearIcon />}
       valueComponent={(valueProps) => {
-          const { value, children, onRemove } = valueProps;
+        const { value, children, onRemove } = valueProps;
 
-          const onDelete = (event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            onRemove(value);
-          };
+        const onDelete = (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          onRemove(value);
+        };
 
-          if (onRemove) {
-            return (
-              <Chip
-                tabIndex={-1}
-                label={children}
-                className={classes.chip}
-                deleteIcon={<CancelIcon onTouchEnd={onDelete} />}
-                onDelete={onDelete}
-              />
-            );
-          }
+        if (onRemove) {
+          return (
+            <Chip
+              tabIndex={-1}
+              label={children}
+              className={classes.chip}
+              deleteIcon={<CancelIcon onTouchEnd={onDelete} />}
+              onDelete={onDelete}
+            />
+          );
+        }
 
-          return <div className="Select-value">{children}</div>;
-        }}
+        return <div className="Select-value">{children}</div>;
+      }}
       {...other}
     />
   );
